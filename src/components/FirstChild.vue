94d2b1ce116d36da9ebe399112cfee742d844c08
kitsune <hatsune.tech@gmail.com>
2020-06-28 21:56:27 +0900

    Propのサンプル追加

--- src/components/FirstChild.vue
+++ src/components/FirstChild.vue
@@ -3,6 +3,8 @@
     <div class="bold">子</div>
     <div>message = {{ message }}</div>
     <div>message2 = {{ message2 }}</div>
+    <div>message3 = {{ message3 }}</div>
+    <div>message4 = {{ message4 }}</div>
     <button @click="changeMessage">messageを直で書き換える</button>
     <button @click="changeMessageByEmit">messageをemitで書き換える</button>
   </div>
@@ -15,6 +17,11 @@ import { Component, Vue, Prop } from "vue-property-decorator";
 export default class FirstChild extends Vue {
   @Prop() message!: string;
   @Prop() message2!: string;
+  // 親からstring型が渡ってくるのでコンソールエラーがでる
+  @Prop(Number) message3!: number;
+  // 親からmessage4は渡されないのでdefaultメッセージが値となる
+  @Prop({ default: 'defaultメッセージ4'}) message4!: number;
+
 
   public changeMessage(): void {
     // 直接書き換えるのはNG
--- src/views/FirstParent.vue
+++ src/views/FirstParent.vue
@@ -4,10 +4,12 @@
     <div class="parent-wrap">
       <div class="bold">親</div>
       <div>message = {{ message }}</div>
+      <div>message3 = {{ message3 }}</div>
       <!-- 変数を展開させたいならコロン付き -->
       <child
         :message="message"
         message2="親からのリテラルメッセージ"
+        :message3="message3"
         @onChangeMessage="changeMessage"
       />
     </div>
@@ -25,6 +27,7 @@ import { default as Child } from "@/components/FirstChild.vue";
 })
 export default class FirstParent extends Vue {
   public message = "親からの変数メッセージ";
+  public message3 = "親からの変数メッセージ3"
 
   public changeMessage(val: string): void {
     // emitを受け取って書き換える
